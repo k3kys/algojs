fce04d4f559092c5336b9c305bad51ca7cbbec00
YEONSOO <m60kys@gmail.com>
2021-03-05 19:10:39 +0900

    dynamic programming 1day

---
+++ reverse-integer/index.js
@@ -0,0 +1,37 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+// Solution#1
+function reverseInt(n) {
+  const reversed = n.toString().split("").reverse().join("");
+
+  if (n < 0) {
+    return parseInt(reversed) * -1;
+  }
+
+  return parseInt(reversed);
+}
+
+// Solution#2
+function reverseInt(n) {
+  const reversed = n.toString().split("").reverse().join("");
+
+  return parseInt(reversed) * Math.sign(n);
+}
+
+// Solution#3
+function reverseInt(n) {
+  let reverseInt = 0;
+  while (n != 0) {
+    reverseInt = reverseInt * 10 + (n % 10);
+    n = parseInt(n / 10);   
+  }
+  return reverseInt;
+}
---
+++ reverse-string/index.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'elppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// Solution#1
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
+
+// Solution#2
+function reverse(str) {
+  let reversed = "";
+
+  for (let characters of str) {
+    reversed = characters + reversed;
+  }
+
+  return reversed;
+}
+
+// Solution#3
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
+
+// Solution#4
+function reverse(str) {
+  return str.split("").reduceRight((reversed, character) => {
+    return reversed + character;
+  }, "");
+}
